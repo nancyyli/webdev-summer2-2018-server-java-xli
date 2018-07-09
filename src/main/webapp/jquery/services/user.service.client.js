0b8ec2f7dc0b9e746e37f5f743af1b7839aa1e92
lixiang <lixiang@ccs.neu.edu>
2018-07-09 19:55:40 -0400

    populated user service methods

--- src/main/webapp/jquery/services/user.service.client.js
+++ src/main/webapp/jquery/services/user.service.client.js
@@ -6,19 +6,46 @@ function UserServiceClient() {
     this.updateUser = updateUser;
     this.url = 'http://localhost:8080/api/user';
     var self = this;
-    function createUser(user, callback) {
-
+    function createUser(user) {
+        return fetch(self.url, {
+            method: 'post',
+            body: JSON.stringify(user),
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
     }
     function findAllUsers(callback) {
-
+        return fetch(self.url)
+        .then(function (response) {
+            return response.json();
+        });
     }
     function findUserById(userId, callback) {
-
+        return fetch(self.url + '/' + userId)
+            .then(function(response){
+                return response.json();
+            });
     }
-    function updateUser(userId, user, callback) {
-
+    function updateUser(userId, user) {
+        return fetch(self.url + '/' + userId, {
+              method: 'put',
+              body: JSON.stringify(user),
+              headers: {
+                  'content-type': 'application/json'
+              }
+          })
+        .then(function(response){
+            if(response.bodyUsed) {
+                return response.json();
+            } else {
+                return null;
+            }
+        });
     }
-    function deleteUser(userId, callback) {
-        
+    function deleteUser(userId) {
+        return fetch(self.url + '/' + userId, {
+          method: 'delete'
+      })
     }
 }
