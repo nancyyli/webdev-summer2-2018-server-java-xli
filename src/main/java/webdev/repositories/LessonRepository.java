52d6cd27bbe0f73cbc1c39d7f8320446cbbcb28d
lixiang <lixiang@ccs.neu.edu>
2018-07-17 18:43:30 -0400

    add LessonRepository.java

---
+++ src/main/java/webdev/repositories/LessonRepository.java
@@ -0,0 +1,17 @@
+package webdev.repositories;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import webdev.models.Lesson;
+import webdev.models.Module;
+
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface LessonRepository extends CrudRepository<Lesson, Integer> {
+
+}
